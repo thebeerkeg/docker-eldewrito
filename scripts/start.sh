80cc718b166f47e1b2110ee1d5574828f6df5453
Warm Beer <mickvd99@gmail.com>
2024-04-22 22:55:25 +0200

    test

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,7 +5,8 @@ GREEN='\033[0;32m'
 RED='\033[0;31m'
 YELLOW='\033[0;33m'
 
-config_file="/config/dewrito_prefs${INSTANCE_ID:+_$INSTANCE_ID}.cfg"
+CONFIG_FILE="dewrito_prefs${INSTANCE_ID:+_$INSTANCE_ID}.cfg"
+CONFIG_FILE_LINK="/config/${CONFIG_FILE}"
 
 echo "Initializing container for ElDewrito Dedicated Server"
 
@@ -18,10 +19,10 @@ create_default_config()
 
     if [ -f "/defaults/${ED_CFG_VERSION}/dewrito_prefs.cfg" ]; then
         echo "Copying default dewrito_prefs.cfg for version: ${ED_CFG_VERSION}."
-        cp "/defaults/${ED_CFG_VERSION}/dewrito_prefs.cfg" "${config_file}"
+        cp "/defaults/${ED_CFG_VERSION}/dewrito_prefs.cfg" "${CONFIG_FILE_LINK}"
         echo "${YELLOW}Make sure to adjust important settings like your RCON password!${NC}"
     else
-        echo "${YELLOW}ElDewrito version unknown. dewrito_prefs.cfg will be generated automatically after running. Make sure to update settings like 'Voting.SystemType'.${NC}"
+        echo "${YELLOW}ElDewrito version unknown. ${CONFIG_FILE} will be generated automatically after running. Make sure to update settings like 'Voting.SystemType'.${NC}"
     fi
 }
 
@@ -70,10 +71,10 @@ else
 fi
 
 # Copy configuration files or create default config
-if [ ! -f "$config_file" ]; then
+if [ ! -f "$CONFIG_FILE_LINK" ]; then
     create_default_config
 else
-    echo "${GREEN}Existing dewrito_prefs.cfg found!${NC}"
+    echo "${GREEN}Found existing config: ${CONFIG_FILE}!${NC}"
 fi
 
 if [ -z "${SKIP_CHOWN}" ]; then
