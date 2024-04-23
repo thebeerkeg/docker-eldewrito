cf9b3ba5502a1d0bf454421486c82e3314919dad
Mick van Dijke <mickvd99@gmail.com>
2024-04-23 03:10:08 +0200

    add 0.7.0 support and easier setup through env variables (#1)

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,7 @@
 FROM ubuntu:22.04
 
 # Set environment variables
-ENV CONTAINER_VERSION=1.0 \
-    ELDEWRITO_VERSION=0.6.1 \
-    MTNDEW_CHECKSUM=496b9296239539c747347805e15d2540 \
-    DISPLAY=:1 \
+ENV DISPLAY=:1 \
     WINEPREFIX="/wine" \
     DEBIAN_FRONTEND=noninteractive \
     PUID=0 \
@@ -13,32 +10,20 @@ ENV CONTAINER_VERSION=1.0 \
 
 # Install temporary packages
 RUN apt-get update && \
-    apt-get install -y wget software-properties-common apt-transport-https cabextract
+    apt-get install -y wget software-properties-common apt-transport-https cabextract xvfb winbind
 
-# Install Wine key and repository
-RUN dpkg --add-architecture i386 && \
-    wget -nc https://dl.winehq.org/wine-builds/winehq.key && \
-    apt-key add winehq.key && \
-    rm winehq.key && \
-    add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main' && \
-    apt-get update
+# Enable 32-bit
+RUN dpkg --add-architecture i386 && apt-get update
 
-# Install Wine stable
-RUN apt-get install -y --install-recommends winehq-stable
-
-# Download winetricks from source
-RUN wget https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks && \
-    chmod +x ./winetricks
-
-# Install X virtual frame buffer and winbind
-RUN apt-get install -y xvfb winbind
+# Install wine and necessary dependencies
+RUN apt-get install -y wine32 winetricks
 
 # Configure wine prefix
 # WINEDLLOVERRIDES is required so wine doesn't ask any questions during setup
 RUN Xvfb :1 -screen 0 320x240x24 & \
     WINEDLLOVERRIDES="mscoree,mshtml=" wineboot -u && \
     wineserver -w && \
-    ./winetricks -q vcrun2012 winhttp
+    winetricks -q vcrun2012 winhttp
 
 #Install libvulkan and libgll
 RUN apt-get install -y libvulkan1:i386 && \
@@ -47,27 +32,10 @@ RUN apt-get install -y libvulkan1:i386 && \
 # Cleanup
 RUN apt-get remove -y wget software-properties-common apt-transport-https cabextract && \
     rm -rf /var/lib/apt/lists/* && \
-    rm winetricks && \
     rm -rf .cache/
-
-# Add the start script
-ADD start.sh .
 
 # Add the default configuration files
 ADD defaults defaults
 
-# Make start script executable and create necessary directories
-RUN chmod +x start.sh && \
-    mkdir config logs
-
-# Set start command to execute the start script
-CMD /start.sh
-
-# Set working directory into the game directory
-WORKDIR /game
-
-# Expose necessary ports
-EXPOSE 11774/udp 11775/tcp 11776/tcp 11777/tcp
-
 # Set volumes
-VOLUME /game /config /logs
+VOLUME /game /scripts /config
---
+++ scripts/start.sh
@@ -0,0 +1,160 @@
+#!/bin/sh
+
+NC='\033[0m'
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+YELLOW='\033[0;33m'
+
+CONFIG_FILE="dewrito_prefs${INSTANCE_ID:+_$INSTANCE_ID}.cfg"
+CONFIG_FILE_LINK="/config/${CONFIG_FILE}"
+
+echo "Initializing container for ElDewrito Dedicated Server"
+
+# Function to create default configuration depending on path
+create_default_config()
+{
+    echo "${YELLOW}Could not find an existing dewrito_prefs.cfg. Trying to use default.${NC}"
+
+    sleep 5
+
+    if [ -f "/defaults/${ED_CFG_VERSION}/dewrito_prefs.cfg" ]; then
+        echo "Copying default dewrito_prefs.cfg for version: ${ED_CFG_VERSION}."
+        cp "/defaults/${ED_CFG_VERSION}/dewrito_prefs.cfg" "${CONFIG_FILE_LINK}"
+        echo "${YELLOW}Make sure to adjust important settings like your RCON password!${NC}"
+    else
+        echo "${YELLOW}ElDewrito version unknown. ${CONFIG_FILE} will be generated automatically after running. Make sure to update settings like 'Voting.SystemType'.${NC}"
+    fi
+}
+
+# Search for eldorado.exe in game directory
+if [ ! -f "eldorado.exe" ]; then
+    echo "${RED}Could not find eldorado.exe. Did you mount the game directory to /game?${NC}"
+
+    sleep 2
+    exit 1
+fi
+
+# Create user if container should run as user
+if [ -z "${RUN_AS_USER}" ]; then
+    echo "Running as root"
+    user=root
+
+    if [ "$PUID" -ne 0 ] || [ "$PGID" -ne 0 ]; then
+        echo "${RED}Tried to set PUID OR PGID without setting RUN_AS_USER.${NC}"
+        echo "${RED}Please set RUN_AS_USER or remove PUID & PGID from your environment variables.${NC}"
+
+        sleep 2
+        exit 40
+    fi
+else
+    echo "Running as eldewrito"
+    user=eldewrito
+
+    if [ "$PUID" -lt 1000 ] || [ "$PUID" -gt 60000 ]; then
+        echo "${RED}PUID is invalid${NC}"
+
+        sleep 2
+        exit 20
+    fi
+
+    if [ "$PGID" -lt 1000 ] || [ "$PGID" -gt 60000 ]; then
+        echo "${RED}PGID is invalid${NC}"
+
+        sleep 2
+        exit 30
+    fi
+
+    if ! id -u eldewrito > /dev/null 2>&1; then
+        echo "Creating user"
+        useradd -u "$PUID" -m -d /tmp/home eldewrito
+    fi
+fi
+
+# Copy configuration files or create default config
+if [ ! -f "$CONFIG_FILE_LINK" ]; then
+    create_default_config
+else
+    echo "${GREEN}Found existing config: ${CONFIG_FILE}!${NC}"
+fi
+
+# Update server settings
+sed -i "s/^UPnP.Enabled \"[^\"]*\"/UPnP.Enabled \"0\"/" "${CONFIG_FILE_LINK}"
+
+if [ -n "${GAME_PORT}" ]; then
+    sed -i "s/^Server.GamePort \"[^\"]*\"/Server.GamePort \"${GAME_PORT}\"/" "${CONFIG_FILE_LINK}"
+fi
+
+if [ -n "${PORT}" ]; then
+    sed -i "s/^Server.Port \"[^\"]*\"/Server.Port \"${PORT}\"/" "${CONFIG_FILE_LINK}"
+fi
+
+if [ -n "${RCON_PORT}" ]; then
+    sed -i "s/^Server.RconPort \"[^\"]*\"/Server.RconPort \"${RCON_PORT}\"/" "${CONFIG_FILE_LINK}"
+fi
+
+if [ -n "${SIGNAL_SERVER_PORT}" ]; then
+    sed -i "s/^Server.SignalServerPort \"[^\"]*\"/Server.SignalServerPort \"${SIGNAL_SERVER_PORT}\"/" "${CONFIG_FILE_LINK}"
+fi
+
+if [ -n "${FILE_SERVER_PORT}" ]; then
+    sed -i "s/^Server.FileServerPort \"[^\"]*\"/Server.FileServerPort \"${FILE_SERVER_PORT}\"/" "${CONFIG_FILE_LINK}"
+fi
+
+if [ -n "${SERVER_NAME}" ]; then
+    sed -i "s/^Server.Name \"[^\"]*\"/Server.Name \"${SERVER_NAME}\"/" "${CONFIG_FILE_LINK}"
+fi
+
+if [ -n "${SERVER_HOST}" ]; then
+    sed -i "s/^Player.Name \"[^\"]*\"/Player.Name \"${SERVER_HOST}\"/" "${CONFIG_FILE_LINK}"
+fi
+
+if [ -n "${RCON_PASSWORD}" ]; then
+    sed -i "s/^Server.RconPassword \"[^\"]*\"/Server.RconPassword \"${RCON_PASSWORD}\"/" "${CONFIG_FILE_LINK}"
+fi
+
+if [ -n "${CHAT_LOG}" ]; then
+    sed -i "s|^Server.ChatLogFile \"[^\"]*\"|Server.ChatLogFile \"${CHAT_LOG}\"|" "${CONFIG_FILE_LINK}"
+else
+    sed -i "s/^Server.ChatLogEnabled \"[^\"]*\"/Server.ChatLogEnabled \"0\"/" "${CONFIG_FILE_LINK}"
+fi
+
+if [ -n "${VOTING_JSON_PATH}" ]; then
+    sed -i "s|^Voting.JsonPath \"[^\"]*\"|Voting.JsonPath \"${VOTING_JSON_PATH}\"|" "${CONFIG_FILE_LINK}"
+fi
+
+if [ -z "${SKIP_CHOWN}" ]; then
+    echo "Taking ownership of folders"
+    chown -R "$PUID":"$PGID" /game /wine
+
+    echo "Changing folder permissions"
+    find /game -type d -exec chmod 775 {} \;
+
+    echo "Changing file permissions"
+    find /game -type f -exec chmod 664 {} \;
+fi
+
+# Xvfb needs cleaning because it doesn't exit cleanly
+echo "Cleaning up"
+rm /tmp/.X1-lock
+
+echo "Starting virtual frame buffer"
+Xvfb :1 -screen 0 320x240x24 &
+
+echo "${GREEN}Starting dedicated server${NC}"
+
+# DLL overrides for Wine are required to prevent issues with master server announcement
+export WINEDLLOVERRIDES="winhttp,rasapi32=b,n"
+
+if [ -n "${WINE_DEBUG}" ]; then
+    echo "Setting wine to verbose output"
+    export WINEDEBUG=warn+all
+fi
+
+if [ -z "${INSTANCE_ID}" ]; then
+    su -c "wine eldorado.exe -launcher -dedicated -window -height 200 -width 200 -minimized" $user
+else
+    echo "Starting instance ${INSTANCE_ID}"
+    su -c "wine eldorado.exe -launcher -dedicated -window -height 200 -width 200 -minimized -instance ${INSTANCE_ID}" $user
+fi
+
+echo "${RED}Server terminated, exiting${NC}"
