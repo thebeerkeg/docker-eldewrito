0545538be438ffd013af0db2910a6301b90318f8
Warm Beer <mickvd99@gmail.com>
2024-04-24 14:30:02 +0200

    test

--- scripts/start.sh
+++ scripts/start.sh
@@ -87,6 +87,7 @@ fi
 
 # Update dewrito_prefs
 update_setting "UPnP.Enabled" "1"
+update_setting "Game.Discord.Enable" "0"
 update_setting "Server.GamePort" "$GAME_PORT"
 update_setting "Server.Port" "$PORT"
 update_setting "Game.RconPort" "$RCON_PORT"
